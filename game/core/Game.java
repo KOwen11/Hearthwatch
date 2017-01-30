dbc6f28e24a0a12838978c90f6d322dd0902c1fc
KevinOwen <kevinowen11@gmail.com>
2017-01-30 01:39:25 -0600

    test commit

---
+++ game/core/Game.java
@@ -0,0 +1,42 @@
+/**
+ * Created by Kevin on 1/29/2017.
+ */
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class Game {
+    private static String state; //none, start turn, draw, play, end turn
+
+
+
+    public static void main(String[] args){
+        Player players = new Player();
+        players.setHp();
+        players.setName("testName");
+        System.out.println(players.getName());
+
+
+
+        System.out.println(players.getName() + "'s pre-shuffled deck: ");
+        int p1i = 0;
+        while (p1i < 30){
+            Card testVar = players.getPlayerDeckList().get(p1i);
+            System.out.println(testVar);
+            p1i = p1i + 1;
+        }
+
+        players.shuffle(players.getPlayerDeck().getCards());
+        System.out.println(players.getName() + "'s post-shuffled deck: ");
+        int p2i = 0;
+        while (p2i < Deck.getMaxDeckSize()){
+            System.out.println(players.getPlayerDeck().getCards().get(p2i));
+            p2i = p2i + 1;
+        }
+    }
+
+}
---
+++ game/core/Test.java
@@ -0,0 +1,14 @@
+import java.util.List;
+
+/**
+ * Created by Kevin on 1/29/2017.
+ */
+public class Test {
+    public static void main(String[] args){
+//        double r = Math.random()*29;
+//        long y = Math.round(r);
+//        int x = (int) y;
+
+
+    }
+}
